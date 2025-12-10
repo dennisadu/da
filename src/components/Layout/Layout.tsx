7774138b55fc9fc123d0b7b7a6ae1f17720e1de1
olek <olek.yemets@gmail.com>
2025-12-10 23:01:30 +0000

    add max width to app container

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import Header from '../Header';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='px-fluid-4/[72px] pt-fluid-4/10'>
+    <div className='app-container px-fluid-4/[72px] pt-fluid-4/10'>
       <Header />
       {children}
     </div>
