77956bc197ee586416a62ccf08175ce828327b2d
olek <olek.yemets@gmail.com>
2026-01-03 22:42:54 +0000

    add button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,60 @@
+import { PropsWithChildren } from 'react';
+import { clsxm } from '@/utils/clsxm';
+import Link from 'next/link';
+
+interface BaseButtonProps extends PropsWithChildren {
+  className?: string;
+}
+
+interface ButtonAsButton extends BaseButtonProps {
+  as?: 'button';
+  type?: 'button' | 'submit' | 'reset';
+  onClick?: () => void;
+  href?: never;
+}
+
+interface ButtonAsLink extends BaseButtonProps {
+  as: 'link';
+  href: string;
+  type?: never;
+  onClick?: never;
+  target?: string;
+  rel?: string;
+}
+
+type ButtonProps = ButtonAsButton | ButtonAsLink;
+
+export const Button = ({
+  children,
+  onClick,
+  className,
+  type = 'button',
+  as = 'button',
+  ...props
+}: ButtonProps) => {
+  const baseStyles = clsxm(
+    '~w-[8rem]/[15rem] ~h-[2.75rem]/[2.875rem] hover:h-[53px] rounded-2xl',
+    'font-helvetica font-bold uppercase',
+    '~text-[1.125rem]/[1.5rem] hover:~text-[1.5rem]/[2rem]',
+    'transition-all duration-300 ease-in-out',
+    'bg-primary-100 text-gray-10',
+    'flex items-center justify-center',
+    'active:scale-95',
+    className,
+  );
+
+  if (as === 'link') {
+    const { href, target, rel } = props as ButtonAsLink;
+    return (
+      <Link href={href} target={target} rel={rel} className={baseStyles}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button type={type} onClick={onClick} className={baseStyles} {...props}>
+      {children}
+    </button>
+  );
+};
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,25 @@
 import Image from 'next/image';
 import heroImg from '@/../public/images/hero_img.jpg';
+import { Button } from '../Button';
+import { CONTACT_EMAIL } from '@/constants';
 
 export const Hero = () => {
   return (
     <div>
-      <h1 className='uppercase ~text-[4.06rem]/[12.5rem] text-primary-100 relative z-10 text-nowrap'>
-        Dennis Adu
-      </h1>
-      <div className='flex justify-center ~-mt-6/14'>
+      <div className='flex'>
+        <h1 className='uppercase ~text-[4.06rem]/[12.5rem] text-primary-100 relative z-10 text-nowrap'>
+          Dennis Adu
+        </h1>
+        <Button
+          as='link'
+          href={`mailto:${CONTACT_EMAIL}`}
+          className='~mt-6/20 hidden sm:flex'
+        >
+          Contact
+        </Button>
+      </div>
+
+      <div className='flex justify-center ~-mt-6/14 relative'>
         <Image
           className='rounded-[clamp(20px,5vw,80px)] ~w-[16.938rem]/[48rem]  h-auto'
           alt='Dennis Adu'
@@ -17,6 +29,13 @@ export const Hero = () => {
           priority
           placeholder='blur'
         />
+        <Button
+          as='link'
+          href={`mailto:${CONTACT_EMAIL}`}
+          className='~mt-6/20 absolute ~/sm:~right-1/12 -bottom-3 sm:hidden'
+        >
+          Contact
+        </Button>
       </div>
     </div>
   );
